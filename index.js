87f9229febebf1c1677650cfdeb94974452e1f58
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2023-07-16 18:36:59 +0100

    Transactions can now spend multiple inputs. Fee rate cannot be set for now.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const ecc = require('tiny-secp256k1')
 const bip39 = require('bip39')
 const { BIP32Factory } = require('bip32')
+const { ECPairFactory } = require('ecpair')
 const bip32 = BIP32Factory(ecc)
 const bitcoin = require('bitcoinjs-lib')
+const assert = require('assert')
+const ECPair = ECPairFactory(ecc)
+const validator = (pubkey, msghash, signature) => {
+  return ECPair.fromPublicKey(pubkey).verify(msghash, signature)
+}
 
 class Wallet {
   constructor (mnemonic) {
@@ -16,6 +22,7 @@ class Wallet {
       receiving: []
     }
     this.addressHistory = {}
+    this.unspentCoins = []
   }
 
   getXprv (mnemonic) {
@@ -69,10 +76,149 @@ class Wallet {
           if (!this.addressHistory[output.addr].includes(tx.hash)) {
             this.addressHistory[output.addr].push(tx.hash)
             this.addressHistory[output.addr].push(tx.block_height)
+            if (!output.spent) {
+              output.hash = tx.hash
+              delete output.spending_outpoints
+              delete output.type
+              this.unspentCoins.push(output)
+            }
           }
         }
       })
     })
+  }
+
+  // const { hash, index, witnessUtxo } = inputData
+  async createTx (receiver, value) {
+    const hdRoot = bip32.fromBase58(this.extendedPrivKey)
+    const inputs = this.getHdData(this.getInputData(value))
+
+    const psbt = new bitcoin.Psbt({ network: bitcoin.networks.bitcoin })
+
+    let totalInputValue = 0
+    inputs.forEach(input => {
+      psbt.addInput(input)
+      totalInputValue +=input.witnessUtxo.value
+    })
+
+    psbt.addOutput({
+      address: receiver,
+      value
+    })
+
+
+
+
+
+
+
+
+//Find a way to adjust fee rate
+
+
+
+
+
+
+
+
+
+    let inputIndex = 0
+    inputs.forEach(input => {
+      psbt.signInputHD(inputIndex, hdRoot)// Must sign with root!!!
+      inputIndex++
+    })
+    let newInputIndex = 0
+    inputs.forEach(input => {
+      const childNode = hdRoot.derivePath(input.bip32Derivation[0].path)
+      assert.strictEqual(psbt.validateSignaturesOfInput(newInputIndex, validator), true)
+      assert.strictEqual(
+        psbt.validateSignaturesOfInput(newInputIndex, validator, childNode.publicKey),
+        true
+      )
+
+      newInputIndex++
+    })
+
+    psbt.finalizeAllInputs()
+
+    const tx = psbt.extractTransaction().toHex()
+    const transaction = bitcoin.Transaction.fromHex(tx);
+const transactionSizeVbytes = transaction.virtualSize();
+console.log(transactionSizeVbytes)
+  }
+
+  // Gathers the right inputs for our transaction. is then passed as an argument to getHdData()
+  getInputData (amount) {
+    let hash = ''
+    let index = 0
+    let addr = ''
+    let mixin = {}
+    const allInputs = []
+    let totalAmount = 0
+    const coin = this.unspentCoins
+    for (let i = 0; i <= coin.length - 1; i++) {
+      if (amount >= coin[i].value) {
+        totalAmount += coin[i].value
+        hash = coin[i].hash
+        index = coin[i].n
+        addr = coin[i].addr
+        const witnessUtxo = this.getWitnessUtxo(coin[i])
+        mixin = { witnessUtxo }
+        allInputs.push({ addr, hash, index, ...mixin })
+        if (totalAmount >= amount) {
+          return allInputs
+        }
+      } else {
+        hash = coin[i].hash
+        index = coin[i].n
+        const witnessUtxo = this.getWitnessUtxo(coin[i])
+        mixin = { witnessUtxo }
+        allInputs.push({ addr, hash, index, ...mixin })
+        return allInputs
+      }
+    }
+  }
+
+  getWitnessUtxo (out) {
+    delete out.addr
+    delete out.spent
+    delete out.n
+    delete out.tx_index
+    out.script = Buffer.from(out.script, 'hex')
+    return out
+  }
+
+  // gets the extended keys needed for each input holding address
+  getHdData (inputData) {
+    const hdRoot = bip32.fromBase58(this.extendedPrivKey)
+    const masterFingerprint = hdRoot.fingerprint
+    inputData.forEach(input => {
+      const pathParts = this.path.split('/')
+      let pathIndex = this.activeAddresses.receiving.indexOf(input.addr)
+      if (pathIndex === -1) {
+        pathParts[1] = 1
+        pathIndex = this.activeAddresses.change.indexOf(input.addr)
+      }
+      pathParts[2] = pathIndex
+      const finalPath = pathParts.join('/')
+      const childNode = hdRoot.derivePath(finalPath)
+      const pubkey = childNode.publicKey
+
+      const updateData = {
+        bip32Derivation: [
+          {
+            masterFingerprint,
+            path: finalPath,
+            pubkey
+          }
+        ]
+      }
+      const { addr, hash, index, witnessUtxo } = input
+      assert.deepStrictEqual({ addr, hash, index, witnessUtxo }, input)
+      Object.assign(input, updateData)
+    })
+    return inputData
   }
 }
 
@@ -82,9 +228,12 @@ const myWallet = new Wallet('main insane wine thank cluster couch word mad flock
 myWallet.generateAddresses(3, false)
 myWallet.generateAddresses(3, true)
 
-
 async function stuff () {
   await myWallet.checkForTxs()
-  console.log(myWallet)
+
+  // myWallet.createTx('bc1qk8g8fszg8kz7ddq2xyudy4hf0x9mxfyvp5vj92', 40000)
+
+  myWallet.createTx('bc1qvxajzx22d9hhq50nrfv3nsfelnjxphq66uz0c8', 25000)
+  // console.log(myWallet.getHdData(myWallet.getInputData(25000))[0].bip32Derivation)
 }
 stuff()
